717d74f7ba8b141e3e803b61111afd7901a06020
mei_yiyi <1596944197@qq.com>
2023-03-04 16:51:55 +0800

    feat:初稿

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,9 +1,13 @@
 const tooltip: HTMLDivElement = document.querySelector('#tooltip')
 
+
+const { width, height } = tooltip.getBoundingClientRect();
 window.updatePosition((ev, { x, y }) => {
-  tooltip.style.left = `${x}px`
-  tooltip.style.top = `${y}px`
-  tooltip.innerHTML = `${x}——${y}`
+  const left = x + width >= window.innerWidth ? `${window.innerWidth - width}px` : `${x}px`
+  const top = y + height >= window.innerHeight ? `${window.innerHeight - height}px` : `${y}px`
+  tooltip.style.left = left
+  tooltip.style.top = top
+  tooltip.innerHTML = `x:${x}\xa0\xa0 y:${y}`
 });
 
 function run() {
